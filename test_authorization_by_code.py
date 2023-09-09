278acbee6ec41e34495d4001cee27ed5bca6daf0
Ekaterina Shirimetova <130868218+justpomadka@users.noreply.github.com>
2023-09-10 01:17:36 +0300

    Create test_authorization_by_code.py

---
+++ test_authorization_by_code.py
@@ -0,0 +1,50 @@
+import pytest
+from .pages.authorization_by_code  import AuthorizationByCodePage
+
+
+
+@pytest.mark.code
+def test_can_open_authorization_by_code_page(browser):
+    link = "https://my.rt.ru/"
+    page = AuthorizationByCodePage(browser, link)
+   
+    page.open()
+  
+    page.should_be_opened_authorization_by_code_page()
+
+
+@pytest.mark.code
+def test_that_code_send_when_telephone_number_entered(browser):
+    link = "https://my.rt.ru/"
+    page = AuthorizationByCodePage(browser, link)
+    
+    page.open()
+
+    phone = "79241545265"
+    page.enter_phone(phone)
+    page.click_get_code_button()
+   
+    page.should_have_message_that_code_was_sent()
+
+
+@pytest.mark.code
+def test_that_code_send_when_email_entered(browser):
+    link = "https://my.rt.ru/"
+    page = AuthorizationByCodePage(browser, link)
+    
+    page.open()
+    email = "mfporubova@gmail.com"
+    page.enter_email(email)
+    page.click_get_code_button()
+   
+    page.should_have_message_that_code_was_sent()
+
+
+@pytest.mark.code
+def test_that_logo_is_presented(browser):
+    link = "https://my.rt.ru/"
+    page = AuthorizationByCodePage(browser, link)
+    
+    page.open()
+    
+    page.should_have_logo()
